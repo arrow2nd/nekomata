a7b2ac86e795a83a89cbc22714bcb94956926cd2
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-03-29 23:50:10 +0900

    feat(api): いろいろやった
    - エラー構造体の仕様変更
    - テストの追加

--- api/mastodon/toot_test.go
+++ api/mastodon/toot_test.go
@@ -1,14 +1,80 @@
-package mastodon
+package mastodon_test
 
 import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
+	"github.com/arrow2nd/nekomata/api"
 	"github.com/arrow2nd/nekomata/api/shared"
 	"github.com/stretchr/testify/assert"
 )
+
+const mockStatus = `
+{
+  "id": "000000",
+  "created_at": "2023-01-01T00:00:00.000Z",
+  "in_reply_to_id": null,
+  "in_reply_to_account_id": null,
+  "sensitive": false,
+  "spoiler_text": "",
+  "visibility": "public",
+  "language": "en",
+  "uri": "https://mastodon.social/users/User/statuses/000000",
+  "url": "https://mastodon.social/@User/000000",
+  "replies_count": 5,
+  "reblogs_count": 6,
+  "favourites_count": 10,
+  "favourited": false,
+  "reblogged": false,
+  "muted": false,
+  "bookmarked": false,
+  "content": "<p>Hello!</p>",
+  "reblog": null,
+  "application": { "name": "nekomata for term", "website": null },
+  "account": {
+    "id": "0",
+    "username": "User",
+    "acct": "User",
+    "display_name": "User",
+    "locked": false,
+    "bot": false,
+    "discoverable": true,
+    "group": false,
+    "created_at": "2023-01-01T00:00:00.000Z",
+    "note": "<p>BIO</p>",
+    "url": "https://mastodon.example.com/@User",
+    "avatar": "https://files.mastodon.example.com/accounts/avatars/000/000/001/original/pic.jpg",
+    "avatar_static": "https://files.mastodon.example.com/accounts/avatars/000/000/001/original/pic.jpg",
+    "header": "https://files.mastodon.example.com/accounts/headers/000/000/001/original/pic.png",
+    "header_static": "https://files.mastodon.example.com/accounts/headers/000/000/001/original/pic.png",
+    "followers_count": 123,
+    "following_count": 456,
+    "statuses_count": 1000,
+    "last_status_at": "2023-01-01T00:00:00.000Z",
+    "emojis": [],
+    "fields": [
+      {
+        "name": "A",
+        "value": "a",
+        "verified_at": null
+      },
+      {
+        "name": "B",
+        "value": "b",
+        "verified_at": "2023-01-01T00:00:00.000+00:00"
+      }
+    ]
+  },
+  "media_attachments": [],
+  "mentions": [],
+  "tags": [],
+  "emojis": [],
+  "card": null,
+  "poll": null
+}`
 
 func TestCreatePost(t *testing.T) {
 	type result struct {
@@ -24,69 +90,7 @@ func TestCreatePost(t *testing.T) {
 		}
 
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintln(w, `
-    {
-      "id": "000000",
-      "created_at": "2023-01-01T00:00:00.000Z",
-      "in_reply_to_id": null,
-      "in_reply_to_account_id": null,
-      "sensitive": false,
-      "spoiler_text": "",
-      "visibility": "public",
-      "language": "en",
-      "uri": "https://mastodon.social/users/User/statuses/000000",
-      "url": "https://mastodon.social/@User/000000",
-      "replies_count": 5,
-      "reblogs_count": 6,
-      "favourites_count": 10,
-      "favourited": false,
-      "reblogged": false,
-      "muted": false,
-      "bookmarked": false,
-      "content": "<p>Hello!</p>",
-      "reblog": null,
-      "application": { "name": "nekomata for term", "website": null },
-      "account": {
-        "id": "0",
-        "username": "User",
-        "acct": "User",
-        "display_name": "User",
-        "locked": false,
-        "bot": false,
-        "discoverable": true,
-        "group": false,
-        "created_at": "2023-01-01T00:00:00.000Z",
-        "note": "<p>BIO</p>",
-        "url": "https://mastodon.example.com/@User",
-        "avatar": "https://files.mastodon.example.com/accounts/avatars/000/000/001/original/pic.jpg",
-        "avatar_static": "https://files.mastodon.example.com/accounts/avatars/000/000/001/original/pic.jpg",
-        "header": "https://files.mastodon.example.com/accounts/headers/000/000/001/original/pic.png",
-        "header_static": "https://files.mastodon.example.com/accounts/headers/000/000/001/original/pic.png",
-        "followers_count": 123,
-        "following_count": 456,
-        "statuses_count": 1000,
-        "last_status_at": "2023-01-01T00:00:00.000Z",
-        "emojis": [],
-        "fields": [
-          {
-            "name": "A",
-            "value": "a",
-            "verified_at": null
-          },
-          {
-            "name": "B",
-            "value": "b",
-            "verified_at": "2023-01-01T00:00:00.000+00:00"
-          }
-        ]
-      },
-      "media_attachments": [],
-      "mentions": [],
-      "tags": [],
-      "emojis": [],
-      "card": null,
-      "poll": null
-    }`)
+		fmt.Fprintln(w, mockStatus)
 	}))
 
 	defer ts.Close()
@@ -95,7 +99,7 @@ func TestCreatePost(t *testing.T) {
 	postVisibility := "public"
 	opts := &shared.CreatePostOpts{Text: postText, Visibility: postVisibility}
 
-	m := &Mastodon{opts: &shared.ClientOpts{Server: ts.URL}}
+	m, _ := api.NewClient(os.Stdout, api.ServiceMastodon, &shared.ClientOpts{Server: ts.URL})
 	res, err := m.CreatePost(opts)
 
 	assert.NoError(t, err)
@@ -110,4 +114,21 @@ func TestCreatePost(t *testing.T) {
 		assert.Equal(t, postText, res.Text)
 		assert.Equal(t, postVisibility, res.Visibility)
 	})
+}
+
+func TestDeletePost(t *testing.T) {
+	id := "012345"
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Contains(t, r.URL.String(), id, "URLに投稿IDが含まれているか")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, mockStatus)
+	}))
+
+	defer ts.Close()
+
+	m, _ := api.NewClient(os.Stdout, api.ServiceMastodon, &shared.ClientOpts{Server: ts.URL})
+	_, err := m.DeletePost(id)
+
+	assert.NoError(t, err)
 }
