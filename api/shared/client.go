dd9cf9d09e09795b157ed73e3f525f26870e6cbd
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-04-30 22:29:54 +0900

    🎨 仮引数の名前を追加

--- api/shared/client.go
+++ api/shared/client.go
@@ -4,25 +4,25 @@ import "io"
 
 type Client interface {
 	// Authenticate : アプリケーション認証を行なってアクセストークンを取得
-	Authenticate(io.Writer) (*User, error)
+	Authenticate(w io.Writer) (*User, error)
 	// GetAnnouncements : サーバーからのお知らせを取得
 	GetAnnouncements() ([]*Announcement, error)
 	// CreatePost : 投稿を作成
-	CreatePost(*CreatePostOpts) (*Post, error)
+	CreatePost(opts *CreatePostOpts) (*Post, error)
 	// QuotePost : 投稿を引用
-	QuotePost(string, *CreatePostOpts) (*Post, error)
+	QuotePost(text string, opts *CreatePostOpts) (*Post, error)
 	// ReplyPost : 投稿に返信
-	ReplyPost(string, *CreatePostOpts) (*Post, error)
+	ReplyPost(text string, opts *CreatePostOpts) (*Post, error)
 	// DeletePost : 投稿を削除
-	DeletePost(string) (*Post, error)
+	DeletePost(id string) (*Post, error)
 	// Reaction : 投稿にリアクション
-	Reaction(string, string) error
+	Reaction(id string, reactionName string) error
 	// UnReaction : リアクションを削除
-	UnReaction(string, string) error
+	UnReaction(id string) error
 	// Repost : 投稿をリポスト
-	Repost(string) error
+	Repost(id string) error
 	// UnRepost : リポストを削除
-	UnRepost(string) error
+	UnRepost(id string) error
 }
 
 // ClientOpts : クライアントの設定
