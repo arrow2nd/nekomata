8512614aafeb0683884283682ab0407f642d46fa
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-04-30 21:58:34 +0900

    💡 コメントを追加

--- api/mastodon/account.go
+++ api/mastodon/account.go
@@ -2,21 +2,35 @@ package mastodon
 
 import "time"
 
+// account : ユーザー情報
 type account struct {
-	ID             string          `json:"id"`
-	Username       string          `json:"username"`
-	Acct           string          `json:"acct"`
-	DisplayName    string          `json:"display_name"`
-	Locked         bool            `json:"locked"`
-	Bot            bool            `json:"bot"`
-	CreatedAt      time.Time       `json:"created_at"`
-	Note           string          `json:"note"`
-	FollowersCount int             `json:"followers_count"`
-	FollowingCount int             `json:"following_count"`
-	StatusesCount  int             `json:"statuses_count"`
-	Fields         []accountFields `json:"fields"`
+	// ID : ユーザーID
+	ID string `json:"id"`
+	// Username : ユーザー名
+	Username string `json:"username"`
+	// Acct : ユーザー名 + ドメイン名からなる文字列 (username@domain)
+	Acct string `json:"acct"`
+	// DisplayName : 表示名
+	DisplayName string `json:"display_name"`
+	// Locked : 非公開アカウントか
+	Locked bool `json:"locked"`
+	// Bot : ボットアカウントか
+	Bot bool `json:"bot"`
+	// CreatedAt : アカウント作成日
+	CreatedAt time.Time `json:"created_at"`
+	// Note : BIO
+	Note string `json:"note"`
+	// FollowersCount : フォロワー数
+	FollowersCount int `json:"followers_count"`
+	// FollowingCount : フォロイー数
+	FollowingCount int `json:"following_count"`
+	// StatusesCount : トゥート数
+	StatusesCount int `json:"statuses_count"`
+	// Fields : カスタムフィールド
+	Fields []accountFields `json:"fields"`
 }
 
+// accountFields : カスタムフィールド
 type accountFields struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
