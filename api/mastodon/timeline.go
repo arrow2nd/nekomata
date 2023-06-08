31edb2f8fce2c9c66315d14498572669406c87ac
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-06-08 21:07:43 +0900

    ✨ (api/mastodon) GetListTimelineを実装

--- api/mastodon/timeline.go
+++ api/mastodon/timeline.go
@@ -58,6 +58,23 @@ func (m *Mastodon) GetHomeTimeline(sinceID string, limit int) ([]*shared.Post, e
 	return statuses2SharedPosts(res), nil
 }
 
-func (m *Mastodon) GetListTimeline(sinceID string, limit int) ([]*shared.Post, error) {
-	return nil, nil
+func (m *Mastodon) GetListTimeline(listID, sinceID string, limit int) ([]*shared.Post, error) {
+	p := url.Values{}
+	p.Add(":list_id", listID)
+
+	endpoint := endpointTImelineList.URL(m.opts.Server, p)
+
+	q := url.Values{}
+	q.Add("limit", strconv.Itoa(limit))
+
+	if sinceID != "" {
+		q.Add("since_id", sinceID)
+	}
+
+	res := []*status{}
+	if err := m.request(http.MethodGet, endpoint, q, true, &res); err != nil {
+		return nil, err
+	}
+
+	return statuses2SharedPosts(res), nil
 }
