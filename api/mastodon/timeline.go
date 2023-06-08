26d034e44c6aff144dfa48209ec6f18627454d56
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-06-08 20:46:57 +0900

    ✨ (api/mastodon) GetLocalTimelineを実装

--- api/mastodon/timeline.go
+++ api/mastodon/timeline.go
@@ -8,12 +8,17 @@ import (
 	"github.com/arrow2nd/nekomata/api/shared"
 )
 
-func (m *Mastodon) GetGlobalTimeline(sinceID string, limit int) ([]*shared.Post, error) {
+func (m *Mastodon) getGlobalTimeline(sinceID string, limit int, local bool) ([]*shared.Post, error) {
 	endpoint := endpointTimelinePublic.URL(m.opts.Server, nil)
 
 	q := url.Values{}
-	q.Add("local", "false")
 	q.Add("limit", strconv.Itoa(limit))
+
+	if local {
+		q.Add("local", "true")
+	} else {
+		q.Add("local", "false")
+	}
 
 	if sinceID != "" {
 		q.Add("since_id", sinceID)
@@ -27,8 +32,12 @@ func (m *Mastodon) GetGlobalTimeline(sinceID string, limit int) ([]*shared.Post,
 	return statuses2SharedPosts(res), nil
 }
 
+func (m *Mastodon) GetGlobalTimeline(sinceID string, limit int) ([]*shared.Post, error) {
+	return m.getGlobalTimeline(sinceID, limit, false)
+}
+
 func (m *Mastodon) GetLocalTimeline(sinceID string, limit int) ([]*shared.Post, error) {
-	return nil, nil
+	return m.getGlobalTimeline(sinceID, limit, true)
 }
 
 func (m *Mastodon) GetHomeTimeline(sinceID string, limit int) ([]*shared.Post, error) {
