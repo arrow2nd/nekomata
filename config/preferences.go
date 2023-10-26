65f5b26725c0cb5964ad02c4a894aeae8e44fe9e
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-10-27 00:05:32 +0900

    ♻️ (config) 調整

--- config/preferences.go
+++ config/preferences.go
@@ -190,7 +190,7 @@ func defaultPreferences() *Preferences {
 			TabMaxWidth:             20,
 		},
 		Template: Template{
-			Post:            "{{ author }}\n{{ text }}\n{{ detail }}\n{{ metrics }}\n",
+			Post:            "{{ author }}\n{{ text }}\n{{ detail }}\n{{ metrics }}",
 			PostAnotation:   "{text} {author_name} {author_username}",
 			PostDetail:      "{{ createdAt }}{{ if .Via }} | via {{ .Via }}{{ end }}",
 			TweetPoll:       "",
