5ae4be8945a5660b3375948b41e4fcba56bcd70c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-11-07 01:04:02 +0900

    👍 (app) 投稿への操作を実装 #43

---
+++ app/action_post.go
@@ -0,0 +1,92 @@
+package app
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/arrow2nd/nekomata/api/sharedapi"
+	"github.com/arrow2nd/nekomata/config"
+)
+
+func (p *postList) action(action string) {
+	target := p.getSelectPost()
+	if target == nil {
+		return
+	}
+
+	id := target.ID
+
+	f := func() {
+		var (
+			err    error           = nil
+			result *sharedapi.Post = nil
+		)
+
+		switch action {
+		case config.ActionReaction:
+			// TODO: リアクション種別が複数ある場合どうにかする (Misskey)
+			result, err = global.client.Reaction(id, "")
+		case config.ActionUnreaction:
+			result, err = global.client.Unreaction(id)
+		case config.ActionRepost:
+			result, err = global.client.Repost(id)
+		case config.ActionUnrepost:
+			result, err = global.client.Unrepost(id)
+		case config.ActionBookmark:
+			result, err = global.client.Bookmark(id)
+		case config.ActionUnbookmark:
+			result, err = global.client.Unbookmark(id)
+		case config.ActionDelete:
+			result, err = global.client.DeletePost(id)
+		}
+
+		if err != nil {
+			global.SetErrorStatus(action, err.Error())
+			return
+		}
+
+		switch action {
+		case config.ActionReaction, config.ActionUnreaction:
+			// TODO: リアクション種別が複数ある場合どうにかする (Misskey)
+			target.Reactions = result.Reactions
+		case config.ActionRepost, config.ActionUnrepost:
+			target.Reposted = result.Reposted
+			target.RepostCount = result.RepostCount
+		case config.ActionBookmark, config.ActionUnbookmark:
+			target.Bookmarked = result.Bookmarked
+		case config.ActionDelete:
+			p.DeletePost(id)
+		}
+
+		// 再描画して反映
+		p.draw(p.getCurrentCursorPos())
+
+		if !strings.HasSuffix(action, "e") {
+			action += "e"
+		}
+
+		global.SetStatus(action+"d", createPostSummary(result))
+	}
+
+	// 確認画面が不要ならそのまま実行
+	if !global.conf.Pref.Confirm[strings.ToLower(action)] {
+		f()
+		return
+	}
+
+	title := fmt.Sprintf(
+		"Do you want to [%s]%s[-:-:-] this post?",
+		global.conf.Style.App.EmphasisText,
+		strings.ToLower(action),
+	)
+
+	global.ReqestPopupModal(&ModalOpts{title, "", f})
+}
+
+func createPostSummary(p *sharedapi.Post) string {
+	return fmt.Sprintf("%s | %s", createUserSummary(p.Author), p.Text)
+}
+
+func createUserSummary(u *sharedapi.Account) string {
+	return fmt.Sprintf("%s @%s", u.DisplayName, u.Username)
+}
